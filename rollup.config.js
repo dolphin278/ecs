ce24a00fbe405ed140f19881d2713eec7c51607e
Boris Egorov <dolphin278@gmail.com>
2020-08-22 17:43:51 +0200

    Prepare demo folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'index.ts',
+export default [{
+  input: 'src/lib/index.ts',
   output: {
     dir: 'output',
     format: 'iife'
   },
   plugins: [typescript()]
-};
+}, {
+  input: 'src/demos/example/index2.ts',
+  output: {
+    dir: 'output/demos/example',
+    format: 'iife'
+  },
+  plugins: [typescript()]
+}];
